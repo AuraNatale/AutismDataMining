fda6b3f829c442e2d063f95994dd3a11bce23d46
Aura <aura.natale@uc.cl>
2024-04-03 10:47:03 +0200

    cambio in prova.py

--- prova.py
+++ prova.py
@@ -5,3 +5,8 @@ print('ciao')
 print('stiamo capendo come fare')
 print('ce la possiamo fareeee')
 print('confido nelle nostre capacità di apprendimento veloce')
+
+
+
+print("dai speriamo funzioniii")
+print("hellooooo")

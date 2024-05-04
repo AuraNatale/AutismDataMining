ecd7d65e413b412e0231f357f77fd3aecfe48f21
Nicoletta Lorusso <n.lorusso1@studenti.unipi.it>
2024-05-04 21:18:06 +0200

    code freshed

--- Source Code/OurFunctions.py
+++ Source Code/OurFunctions.py
@@ -5,7 +5,7 @@ import matplotlib.patches as mpatches
 '''The function select_columns(df) is useful for selecting and distinguishing
 numerical features and categorical ones. In order to use it the framework is:
 
-numeric_columns, categorical_columns = select_columns(our_dataset_inuse)'''
+numeric_columns, categorical_columns, df = select_columns(our_dataset_inuse)'''
 
 def select_columns(df):
     # Seleziona le colonne numeriche
@@ -20,7 +20,7 @@ def select_columns(df):
     # Seleziona solo le colonne di tipo 'category'
     categorical_columns = df.select_dtypes(include=['category']).columns
     
-    return numeric_columns, categorical_columns
+    return numeric_columns, categorical_columns, df
 
 '''The fanction plot_missing_values() is useful to plot features, devided into 
 categorical and numerical, according to the number of missing values. In order to use it:
@@ -28,7 +28,7 @@ categorical and numerical, according to the number of missing values. In order t
 plot_missing_values(columns_with_nan_sorted, numeric_columns)'''
 
 
-def plot_missing_values(columns_with_nan_sorted, numeric_columns):
+def plot_missing_values(columns_with_nan_sorted, numeric_columns, legend):
     # Assegna colori alle colonne
     '''Utilizza la lista numeric_columns per determinare se una colonna è numerica
     o meno, e assegna il colore blu alle colonne numeriche e arancione alle colonne
@@ -49,13 +49,17 @@ def plot_missing_values(columns_with_nan_sorted, numeric_columns):
     orange_patch = mpatches.Patch(color='orange', label='Categorical Columns')
     plt.legend(handles=[blue_patch, orange_patch])
 
-    plt.show()
+     # Se la legenda non è richiesta, non mostrare la legenda
+    if not legend:
+        plt.legend().remove()
+
+    
 
 '''stavolta riferito al numero di valori presenti
 Utilizzo della funzione
 plot_features_by_presence(columns_by_presence, numeric_columns)'''
 
-def plot_features_by_presence(columns_by_presence, numeric_columns):
+def plot_features_by_presence(columns_by_presence, numeric_columns, legend):
     # Assegna colori alle colonne
     colors = ['blue' if col in numeric_columns else 'orange' for col in columns_by_presence.index]
 
@@ -72,6 +76,10 @@ def plot_features_by_presence(columns_by_presence, numeric_columns):
     orange_patch = mpatches.Patch(color='orange', label='Categorical Columns')
     plt.legend(handles=[blue_patch, orange_patch])
 
-    plt.show()
+    # Se la legenda non è richiesta, non mostrare la legenda
+    if not legend:
+        plt.legend().remove()
+
+    
 
 
